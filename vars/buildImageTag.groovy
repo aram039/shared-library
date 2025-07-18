3b9434f90afa85df479a8e663f41978bfb996898
Arun <144141652+aram039@users.noreply.github.com>
2025-07-18 18:12:30 +0530

    Update buildImageTag.groovy

--- vars/buildImageTag.groovy
+++ vars/buildImageTag.groovy
@@ -1,11 +1,25 @@
+#!/usr/bin/env groovy
+
 def call() {
-    def tagDate = sh(script: "git show -s --format=%ci | cut -d ':' -f1-2 | tr ' ' 'r' | tr -d - | tr -d :", returnStdout: true).trim()
-
-    def branch = env.BRANCH_NAME ?: 'unknown-branch'
-    def branchName = branch.contains('/') ? branch.substring(branch.indexOf('/') + 1) : branch
-    branchName = branchName.replaceAll('/', '_').take(40)
-
-    def commitHash = sh(script: "git rev-parse HEAD | cut -c1-7", returnStdout: true).trim()
-
-    return "${tagDate}-${branchName}_${commitHash}"
+    // Get timestamp in YYYYMMDD-HHMM format from git commit
+    def tagDate = sh(
+        script: "git show -s --format=%ci | cut -d ':' -f1-2 | tr ' ' 'T' | tr -d '-' | tr -d ':'", 
+        returnStdout: true
+    ).trim()
+    
+    // Process branch name (remove prefix, replace slashes, limit length)
+    def branchName = env.BRANCH_NAME.substring(env.BRANCH_NAME.indexOf('/') + 1)
+                          .replaceAll('/', '_')
+                          .take(40)
+    
+    // Get short commit hash
+    def commitHash = sh(
+        script: "git rev-parse HEAD | cut -c1-7", 
+        returnStdout: true
+    ).trim()
+    
+    // Combine components into final tag
+    def imageTag = "${tagDate}-${branchName}_${commitHash}"
+    
+    return imageTag
 }
