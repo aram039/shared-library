c911076ecbbba6b41e0b496b485bb8789112ce19
Arun <144141652+aram039@users.noreply.github.com>
2025-07-18 23:25:50 +0530

    removed unwanted lines

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -26,37 +26,3 @@ def call(Map args) {
         docker push ${ecrImage}
     """
 }
-
-
-
-// def call(Map args) {
-//     def imageTag   = args.imageTag
-//     def contextDir = args.contextDir ?: '.'
-//     def imageName  = args.imageName
-//     def dockerfile = args.dockerfile ?: 'Dockerfile'
-//     def awsRegion  = args.awsRegion ?: 'eu-west-1'
-//     def accountId  = args.accountId ?: '513296374752'
-//     def preBuild   = args.preBuild ?: ''
-//     def fullImageName = "${imageName}:${imageTag}"
-//     def ecrImage = "${accountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}:${imageTag}"
-
-//     // Make sure Dockerfile path is relative to the contextDir
-//     def dockerfilePath = "${contextDir}/${dockerfile}"
-
-//     sh """
-//         set -e
-
-//         echo "Running pre-build commands (if any)..."
-//         ${preBuild}
-
-//         echo "Building Docker image: ${fullImageName}"
-//         docker build -f "${dockerfilePath}" -t "${fullImageName}" "${contextDir}"
-
-//         echo "Tagging image for ECR: ${ecrImage}"
-//         docker tag "${fullImageName}" "${ecrImage}"
-
-//         echo "Pushing image to ECR: ${ecrImage}"
-//         docker push "${ecrImage}"
-//     """
-// }
-
