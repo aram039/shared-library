0529d2846805da1c01e72a177dd5e4d419f3fb2f
Arun <144141652+aram039@users.noreply.github.com>
2025-07-18 23:12:18 +0530

    contextDir removed

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -1,32 +1,3 @@
-// def call(Map args) {
-//     def imageTag   = args.imageTag
-//     def contextDir = args.contextDir ?: '.'
-//     def imageName  = args.imageName
-//     def dockerfile = args.dockerfile ?: 'Dockerfile'
-//     def awsRegion  = args.awsRegion ?: 'eu-west-1'
-//     def accountId  = args.accountId ?: '513296374752'
-//     def preBuild   = args.preBuild ?: ''
-//     def fullImageName = "${imageName}:${imageTag}"
-//     def ecrImage = "${accountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}:${imageTag}"
-
-//     sh """
-//         set +x
-//         cd ${contextDir}
-
-//         echo "Running pre-build commands (if any)..."
-//         ${preBuild}
-
-//         echo "Building Docker image: ${fullImageName}"
-//         docker build -f ${dockerfile} -t ${fullImageName} .
-
-//         echo "Tagging image for ECR: ${ecrImage}"
-//         docker tag ${fullImageName} ${ecrImage}
-
-//         echo "Pushing image to ECR: ${ecrImage}"
-//         docker push ${ecrImage}
-//     """
-// }
-
 def call(Map args) {
     def imageTag   = args.imageTag
     def contextDir = args.contextDir ?: '.'
@@ -38,23 +9,54 @@ def call(Map args) {
     def fullImageName = "${imageName}:${imageTag}"
     def ecrImage = "${accountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}:${imageTag}"
 
-    // Make sure Dockerfile path is relative to the contextDir
-    def dockerfilePath = "${contextDir}/${dockerfile}"
-
     sh """
-        set -e
+        set +x
+        cd ${contextDir}
 
         echo "Running pre-build commands (if any)..."
         ${preBuild}
 
         echo "Building Docker image: ${fullImageName}"
-        docker build -f "${dockerfilePath}" -t "${fullImageName}" "${contextDir}"
+        docker build -f ${dockerfile} -t ${fullImageName} .
 
         echo "Tagging image for ECR: ${ecrImage}"
-        docker tag "${fullImageName}" "${ecrImage}"
+        docker tag ${fullImageName} ${ecrImage}
 
         echo "Pushing image to ECR: ${ecrImage}"
-        docker push "${ecrImage}"
+        docker push ${ecrImage}
     """
 }
+
+
+
+// def call(Map args) {
+//     def imageTag   = args.imageTag
+//     def contextDir = args.contextDir ?: '.'
+//     def imageName  = args.imageName
+//     def dockerfile = args.dockerfile ?: 'Dockerfile'
+//     def awsRegion  = args.awsRegion ?: 'eu-west-1'
+//     def accountId  = args.accountId ?: '513296374752'
+//     def preBuild   = args.preBuild ?: ''
+//     def fullImageName = "${imageName}:${imageTag}"
+//     def ecrImage = "${accountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}:${imageTag}"
+
+//     // Make sure Dockerfile path is relative to the contextDir
+//     def dockerfilePath = "${contextDir}/${dockerfile}"
+
+//     sh """
+//         set -e
+
+//         echo "Running pre-build commands (if any)..."
+//         ${preBuild}
+
+//         echo "Building Docker image: ${fullImageName}"
+//         docker build -f "${dockerfilePath}" -t "${fullImageName}" "${contextDir}"
+
+//         echo "Tagging image for ECR: ${ecrImage}"
+//         docker tag "${fullImageName}" "${ecrImage}"
+
+//         echo "Pushing image to ECR: ${ecrImage}"
+//         docker push "${ecrImage}"
+//     """
+// }
 
