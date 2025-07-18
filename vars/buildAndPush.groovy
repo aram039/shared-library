e8dd05481a8204f2beab3e72e1bde8b133672a03
Arun <144141652+aram039@users.noreply.github.com>
2025-07-18 16:45:09 +0530

    Update buildAndPush.groovy

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -1,6 +1,14 @@
-import org.yourorg.docker.Builder
-
 def call(Map args = [:]) {
-    def builder = new Builder(this)
-    builder.buildAndPushImage(args)
+
+    def imageName   = args.get('imageName', 'default-app')
+    def imageTag    = args.get('imageTag', 'latest')
+    def registry    = args.get('registry', '513296374752.dkr.ecr.eu-west-1.amazonaws.com')
+    def repoPath    = args.get('repoPath', "trustd/app")
+    def imagePath   = "${registry}/${repoPath}/${imageName}:${imageTag}"
+
+    sh "docker build -t ${imagePath} ."
+    sh "docker push ${imagePath}"
+
+    echo "Pushed Docker image: ${imagePath}"
+    return imagePath
 }
