c426ffb9102269d5b42ca1dcf2a3047768e0e619
Arun <144141652+aram039@users.noreply.github.com>
2025-07-18 18:11:46 +0530

    Update buildAndPush.groovy

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -22,8 +22,8 @@ def call(Map args) {
         echo "Tagging image for ECR: ${ecrImage}"
         docker tag ${fullImageName} ${ecrImage}
 
-        echo "Logging in to AWS ECR"
-        aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${accountId}.dkr.ecr.${awsRegion}.amazonaws.com
+        // echo "Logging in to AWS ECR"
+        // aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${accountId}.dkr.ecr.${awsRegion}.amazonaws.com
 
         echo "Pushing image to ECR: ${ecrImage}"
         docker push ${ecrImage}
