e906b4b62f057a9cc367d73b15d30b3e92df4272
Arun <144141652+aram039@users.noreply.github.com>
2025-07-18 18:20:14 +0530

    Update buildImageTag.groovy

--- vars/buildImageTag.groovy
+++ vars/buildImageTag.groovy
@@ -1,21 +1,25 @@
 #!/usr/bin/env groovy
 
 def call() {
+
     def tagDate = sh(
         script: "git show -s --format=%ci | cut -d ':' -f1-2 | tr ' ' 'T' | tr -d '-' | tr -d ':'", 
         returnStdout: true
     ).trim()
     
-    // Process branch name (remove prefix, replace slashes, limit length)
-    def branchName = env.BRANCH_NAME.substring(env.BRANCH_NAME.indexOf('/') + 1)
-                          .replaceAll('/', '_')
-                          .take(40)
-    
+    def branchName = "unknown"
+    if (env.BRANCH_NAME) {
+
+        def parts = env.BRANCH_NAME.split('/')
+        branchName = parts[-1]  // Get last part after splitting
+    }
+    branchName = branchName.replaceAll('/', '_').take(40)    
+
     def commitHash = sh(
         script: "git rev-parse HEAD | cut -c1-7", 
         returnStdout: true
     ).trim()
-
+    
     def imageTag = "${tagDate}-${branchName}_${commitHash}"
     
     return imageTag
