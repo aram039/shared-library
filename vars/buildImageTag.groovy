221e403edc922e2323bb58c64480abadb87e1a14
Arun <144141652+aram039@users.noreply.github.com>
2025-07-18 17:38:54 +0530

    Update buildImageTag.groovy

--- vars/buildImageTag.groovy
+++ vars/buildImageTag.groovy
@@ -1,13 +1,9 @@
 def call() {
     def tagDate = sh(script: "git show -s --format=%ci | cut -d ':' -f1-2 | tr ' ' 'r' | tr -d - | tr -d :", returnStdout: true).trim()
 
-    def branchName = env.BRANCH_NAME ?: sh(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim()
-    if (branchName.contains('/')) {
-        branchName = branchName.substring(branchName.indexOf('/') + 1)
-    }
-    branchName = branchName.replaceAll('/', '_').take(40)
+    def branchName = env.BRANCH_NAME.substring(env.BRANCH_NAME.indexOf('/') + 1).replaceAll('/', '_').take(40)
 
-    def commitHash = sh(script: "git rev-parse HEAD | cut -c1-7", returnStdout: true).trim()
+    def ccommitHash = sh(script: "git rev-parse HEAD | cut -c1-7", returnStdout: true).trim()
 
     return "${tagDate}-${branchName}_${commitHash}"
 }
