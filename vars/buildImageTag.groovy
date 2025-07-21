75f6b2f8c731dcebcbb3e53076d7693ce283da2a
Arun <144141652+aram039@users.noreply.github.com>
2025-07-21 18:55:37 +0530

    Update buildImageTag.groovy

--- vars/buildImageTag.groovy
+++ vars/buildImageTag.groovy
@@ -1,26 +1,10 @@
-// def call() {
-//     def tagDate = sh(script: "git show -s --format=%ci | cut -d ':' -f1-2 | tr ' ' 'r' | tr -d - | tr -d :", returnStdout: true).trim()
-
-//     def branch = params.BRANCH ?: env.BRANCH ?: BRANCH_NAME
-//     if (!branch || branch.trim() == '') {
-//         branch = sh(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim()
-//     }
-
-//     def branchName = branch.contains('/') ? branch.substring(branch.indexOf('/') + 1) : branch
-//     branchName = branchName.replaceAll('/', '_').take(40)
-
-//     def commitHash = sh(script: "git rev-parse HEAD | cut -c1-7", returnStdout: true).trim()
-
-//     def imageTag = "${tagDate}-${branchName}_${commitHash}"
-
-//     echo "Generated Docker image tag: ${imageTag}"
-    
-//     return imageTag
-// }
 def call() {
     def tagDate = sh(script: "git show -s --format=%ci | cut -d ':' -f1-2 | tr ' ' 'r' | tr -d - | tr -d :", returnStdout: true).trim()
 
-    def branch = params.BRANCH ?: env.BRANCH ?: env.GIT_BRANCH ?: sh(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim()
+    def branch = params.BRANCH ?: env.BRANCH ?: BRANCH_NAME
+    if (!branch || branch.trim() == '') {
+        branch = sh(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim()
+    }
 
     def branchName = branch.contains('/') ? branch.substring(branch.indexOf('/') + 1) : branch
     branchName = branchName.replaceAll('/', '_').take(40)
@@ -33,3 +17,4 @@ def call() {
     
     return imageTag
 }
+
